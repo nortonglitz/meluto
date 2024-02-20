9e8ae90743417931c13b2514f11375462f0f2d99
Norton Glitz <norton.glitz@gmail.com>
2024-02-20 02:29:17 -0300

    create finduserbyemail query

---
+++ postgres/queries/users.ts
@@ -0,0 +1,14 @@
+import { users } from "../schemas"
+import { postgres } from ".."
+import { eq, sql } from "drizzle-orm"
+
+export const findUserByEmail = (email: string) => {
+
+    const query = postgres
+        .select()
+        .from(users)
+        .where(eq(users.email, sql.placeholder("email")))
+        .prepare("find_user_by_email")
+
+    return query.execute({ email: email })
+}
