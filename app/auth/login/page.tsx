eda4992cd2b6de24b3ccc2c1e325fbe9de48f337
Norton Glitz <norton.glitz@gmail.com>
2024-02-20 01:10:36 -0300

    insert logo on login

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,11 +1,15 @@
 import { SocialMediaButton } from "./social-media-button"
 import { LoginForm } from "./login-form"
+import { Logo } from "@/components"
 
 const LoginPage = () => {
 
     return (
         <div className="flex justify-center flex-grow items-center">
-            <div className="max-w-[20vw] animate-fade-in">
+            <div className="max-w-[20vw] animate-fade-in relative">
+                <div className="absolute -top-[30%] flex justify-center">
+                    <Logo className="w-[60%]" />
+                </div>
                 <h1
                     className="
                             left-6
@@ -17,8 +21,7 @@ const LoginPage = () => {
                 >
                     Entrar
                 </h1>
-                <p className="text-gray-400">Estamos felizes por você estar aqui. Entre para ter acesso a todo o nosso conteúdo.</p>
-                <hr className="mb-5 mt-4 border-gray-200" />
+                <p className="text-gray-400 mb-4">Estamos felizes por você estar aqui. Entre para ter acesso a todo o nosso conteúdo.</p>
                 <LoginForm />
                 <div className="flex my-4 items-center text-xs">
                     <div className="flex-grow border-t border-gray-200"></div>
