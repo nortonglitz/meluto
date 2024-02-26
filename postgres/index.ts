69602f76456c0bc2df5b09f4e563e9f3776b4317
Norton Glitz <norton.glitz@gmail.com>
2024-02-26 17:24:00 -0300

    change folder

--- postgres/index.ts
+++ postgres/index.ts
@@ -1,7 +1,7 @@
 import { drizzle } from "drizzle-orm/vercel-postgres"
 import { sql } from "@vercel/postgres"
-import * as schemas from "./schemas"
+import * as tables from "./tables"
 
 // If is not possible to find a table, 
 // make sure the table is exported in index.ts from schemas folder
-export const postgres = drizzle(sql, { schema: schemas })
+export const postgres = drizzle(sql, { schema: tables })
