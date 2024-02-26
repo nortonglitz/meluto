e165891a2e713c44fa87a3dff136ed0dd45796e2
Norton Glitz <norton.glitz@gmail.com>
2024-02-26 18:38:31 -0300

    use dark theme

--- components/topbar/avatar.tsx
+++ components/topbar/avatar.tsx
@@ -1,3 +1,5 @@
+import { FaUser } from "react-icons/fa"
+
 interface AvatarProps {
     src: string | null | undefined
     loading?: boolean
@@ -7,23 +9,49 @@ export const Avatar = ({
     src,
     loading = false
 }: AvatarProps) => {
-    return (
-        <img
-            className={`
-                relative
-                transition-all
-                rounded-full
-                h-8
-                cursor-pointer
-                outline-gray-100
 
+    return (
+        <div
+            className={`
+                transition-all
+                h-9
+                w-9
+                rounded-full
+                flex
+                justify-center
+                items-center
+                cursor-pointer
+                outline-scooter-300
+
+                hover:outline-2
                 hover:outline
-                hover:outline-4
+
+                dark:bg-gray-800
+
+                bg-gray-200
 
                 ${loading && "animate-pulse"}
             `}
-            src={src ?? "/images/placeholders/blank-profile.webp"}
-            alt="profile picture"
-        />
+        >
+            {src ?
+                <img
+                    className={`
+                        rounded-full
+                        ${loading && "animate-pulse"}
+                    `}
+                    src={src}
+                    alt="profile image"
+                />
+                :
+                <FaUser
+                    className="
+                        h-5
+                        w-5
+                        dark:text-scooter-200
+                        text-scooter-900
+                    "
+                />
+            }
+        </div>
     )
 }
