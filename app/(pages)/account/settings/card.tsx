122cf5039235f7d693a07d0595d281df67b66a51
Norton Glitz <norton.glitz@gmail.com>
2024-02-27 14:38:33 -0300

    use link on card

--- app/(pages)/account/settings/card.tsx
+++ app/(pages)/account/settings/card.tsx
@@ -1,46 +1,51 @@
 import { Description } from "@/components"
+import Link from "next/link"
 
 interface Card {
     icon: React.ReactNode
     title: string
     desc: string
+    href: string
 }
 
 export const Card = ({
     icon,
     title,
-    desc
+    desc,
+    href
 }: Card) => {
     return (
-        <div
-            className="
-                    select-none
-                    w-full
-                    sm:max-w-96
-                    transition
-                    rounded-xl
-                    border
-                    p-4
-                    cursor-pointer
-
-                    hover:bg-gray-100
-                    border-gray-200
-                    
-                    dark:border-gray-700
-                    dark:hover:bg-gray-900
-                "
-        >
+        <Link href={href}>
             <div
                 className="
-                    flex
-                    mb-4
-                    [&>svg]:text-3xl
-                    [&>svg]:mr-3
-                ">
-                {icon}
-                <h2 className="font-exo font-medium text-lg">{title}</h2>
+                        select-none
+                        w-full
+                        sm:max-w-96
+                        transition
+                        rounded-xl
+                        border
+                        p-4
+                        cursor-pointer
+
+                        hover:bg-gray-100
+                        border-gray-200
+                        
+                        dark:border-gray-700
+                        dark:hover:bg-gray-900
+                    "
+            >
+                <div
+                    className="
+                        flex
+                        mb-4
+                        [&>svg]:text-3xl
+                        [&>svg]:mr-3
+                    ">
+                    {icon}
+                    <h2 className="font-exo font-medium text-lg">{title}</h2>
+                </div>
+                <Description className="text-gray-500 dark:text-gray-400">{desc}</Description>
             </div>
-            <Description className="text-gray-500 dark:text-gray-400">{desc}</Description>
-        </div>
+        </Link>
     )
 }
