b1d6a71a4baeed099aadb71dd76a5aff323605af
Norton Glitz <norton.glitz@gmail.com>
2024-02-27 14:38:44 -0300

    use href on cards

--- app/(pages)/account/settings/page.tsx
+++ app/(pages)/account/settings/page.tsx
@@ -1,5 +1,11 @@
 import { FaAddressCard, FaRankingStar, FaWallet } from "react-icons/fa6"
 import { Card } from "./card"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: "Conta ─ Meluto",
+    description: "Encontre todos seus detalhes no nosso site."
+}
 
 export default function AccountSettingsPage() {
     return (
@@ -12,16 +18,19 @@ export default function AccountSettingsPage() {
                     title="Informações Pessoais"
                     desc="Gerencie telefone, e-mail e outros detalhes sobre você."
                     icon={<FaAddressCard />}
+                    href="/account/settings/personal"
                 />
                 <Card
                     title="Financeiro"
                     desc="Veja suas formas de pagamento, endereço de faturamento e outros detalhes."
                     icon={<FaWallet />}
+                    href="#"
                 />
                 <Card
                     title="Plano"
                     desc="Saiba mais sobre outros planos, sobre a sua assinatura e outros detalhes."
                     icon={<FaRankingStar />}
+                    href="#"
                 />
             </div>
             {/* <div
