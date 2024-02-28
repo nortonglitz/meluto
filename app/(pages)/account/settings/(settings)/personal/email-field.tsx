4df585597f3ce9edfb51d4a28cd0c686d131c00f
Norton Glitz <norton.glitz@gmail.com>
2024-02-28 15:17:10 -0300

    create email field to use on display data

---
+++ app/(pages)/account/settings/(settings)/personal/email-field.tsx
@@ -0,0 +1,64 @@
+"use client"
+
+import { InputText } from "@/components"
+import { ButtonFeedback } from "@/components"
+import { EmailSchema, useEmailSchema } from "@/validations/schemas/auth"
+
+interface EmailFieldProps {
+    initialValue: string
+    onCancel: () => void
+}
+
+export const EmailField = ({
+    initialValue,
+    onCancel
+}: EmailFieldProps) => {
+
+    const { handleSubmit, register, formState: { errors } } = useEmailSchema({
+        defaultValues: {
+            email: initialValue
+        }
+    })
+
+    const onSubmit = (values: EmailSchema) => {
+        console.log(values)
+    }
+
+    return (
+        <form
+            className="
+                my-4
+                flex
+                gap-4
+                flex-col
+                w-full
+                
+                sm:w-fit
+                sm:relative
+            "
+            onSubmit={handleSubmit(onSubmit)}
+        >
+            <InputText
+                autoFocus
+                className="w-full"
+                {...register("email")}
+                error={errors.email?.message}
+            />
+            <div
+                className="
+                    flex
+                    w-full
+                    justify-around
+                    ml-2
+                    
+                    sm:absolute
+                    sm:left-full
+                    sm:justify-normal
+                "
+            >
+                <ButtonFeedback option="cancel" className="mr-2" onClick={() => onCancel()} />
+                <ButtonFeedback option="confirm" type="submit" />
+            </div>
+        </form>
+    )
+}
