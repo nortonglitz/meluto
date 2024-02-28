c7e7175e13b8aeefcb5f7faf5a8eafa1bc685c21
Norton Glitz <norton.glitz@gmail.com>
2024-02-28 13:54:55 -0300

    enable input on edit data on data display component

--- app/(pages)/account/settings/(settings)/personal/data-display.tsx
+++ app/(pages)/account/settings/(settings)/personal/data-display.tsx
@@ -1,4 +1,7 @@
+"use client"
+
 import { Description } from "@/components"
+import { useState } from "react"
 import { FaEdit } from "react-icons/fa"
 
 interface DataDisplayProps {
@@ -6,43 +9,53 @@ interface DataDisplayProps {
     subtitle: string
     value: string
     lastEdit: string
+    editComponent: React.ReactNode
 }
 
 export const DataDisplay = ({
     title,
     subtitle,
     value,
-    lastEdit
+    lastEdit,
+    editComponent
 }: DataDisplayProps) => {
+
+    const [showEditComponent, setShowEditComponent] = useState(false)
+
+    const displayValueButton = (
+        <button
+            onClick={() => setShowEditComponent(true)}
+            className="
+                border
+                w-fit
+                my-4
+                border-dashed
+                rounded-3xl
+                py-2
+                px-4
+                cursor-pointer
+                flex
+                items-center
+                
+                border-gray-400
+                hover:bg-gray-100
+
+                dark:hover:bg-gray-900
+                dark:border-gray-500
+            "
+        >
+            {value}
+            <FaEdit className="ml-2" />
+        </button>
+    )
+
     return (
         <div>
             <h6 className="text-xl font-exo font-semibold">{title}</h6>
             <p className="mb-2">
                 {subtitle}
             </p>
-            <button
-                className="
-                    border
-                    w-fit
-                    px-2
-                    py-1
-                    mb-1
-                    border-dashed
-                    rounded-xl
-                    cursor-pointer
-                    flex
-                    items-center
-                    
-                    border-gray-400
-                    hover:bg-gray-100
-
-                    dark:hover:bg-gray-900
-                    dark:border-gray-500
-                "
-            >
-                {value}
-                <FaEdit className="ml-2" />
-            </button>
+            {showEditComponent ? editComponent : displayValueButton}
             <Description className="text-sm">
                 <i>
                     Atualizado em {lastEdit}
