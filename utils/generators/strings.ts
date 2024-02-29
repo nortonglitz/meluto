6ec8f3f1faa2829f23f408a6d042b88876393548
Norton Glitz <norton.glitz@gmail.com>
2024-02-29 01:01:36 -0300

    create string generator

---
+++ utils/generators/strings.ts
@@ -0,0 +1,8 @@
+export const generateRandomString = (length: number) => {
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    let result = ""
+    for (let i = 0; i < length; i++) {
+        result += chars.charAt(Math.floor(Math.random() * chars.length))
+    }
+    return result
+}
