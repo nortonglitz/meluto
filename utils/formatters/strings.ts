b6488574ff9f374e967a9d9110dafe978539b9d4
Norton Glitz <norton.glitz@gmail.com>
2024-02-29 01:41:06 -0300

    formatter accept null and undefined

--- utils/formatters/strings.ts
+++ utils/formatters/strings.ts
@@ -1,4 +1,8 @@
-export const formatPhone = (phone: string) => {
+export const formatPhone = (phone: string | undefined | null) => {
+    if (!phone) {
+        return ""
+    }
+
     switch (phone.length) {
         case 10:
             return `(${phone.substring(0, 2)}) ${phone.substring(2, 6)}-${phone.substring(6, 10)}`
