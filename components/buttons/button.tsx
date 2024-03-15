03fc0e2a11d1cab530a35afd8c19c9ca985fb313
Norton Glitz <norton.glitz@gmail.com>
2024-03-14 23:19:59 -0300

    button include new variant

--- components/buttons/button.tsx
+++ components/buttons/button.tsx
@@ -2,23 +2,24 @@ import { DetailedHTMLProps, ButtonHTMLAttributes } from "react"
 
 interface ButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
     children?: React.ReactNode
-    link?: boolean
     color?: "primary" | "secondary" | "success" | "neutral"
+    variant?: "outlined" | "standard" | "text"
 }
 
 export const Button = ({
     children,
-    link = false,
     color = "primary",
     className,
+    variant = "standard",
     ...props
 }: ButtonProps) => {
 
     const isPrimary = color === "primary"
     const isSecondary = color === "secondary"
     const isSuccess = color === "success"
+    const isNeutral = color === "neutral"
 
-    if (link) {
+    if (variant === "text") {
         return (
             <button
                 className={`
@@ -44,6 +45,41 @@ export const Button = ({
         )
     }
 
+    if (variant === "outlined") {
+        return (
+            <button
+                className={`
+                    font-medium
+                    px-4
+                    py-2
+                    rounded-3xl
+
+                    outline
+
+                    focus-visible:outline-2
+                    dark:focus-visible:outline-white
+                    
+                    hover:bg-gray-400/10
+                    active:bg-gray-400/20
+
+                    dark:hover:bg-gray-400/20
+                    dark:active:bg-gray-400/30
+                    
+                    
+                    ${isPrimary ? "text-scooter-300" : ""}
+                    ${isSecondary ? "text-trinidad-500" : ""}
+                    ${isSuccess ? "text-green-500" : ""}
+                    ${isNeutral ? "text-inherit" : ""}
+
+                    ${className ? className : ""}
+                `}
+                {...props}
+            >
+                {children}
+            </button>
+        )
+    }
+
     return (
         <button
             className={`
@@ -55,20 +91,35 @@ export const Button = ({
                 disabled:opacity-50
                 disabled:cursor-not-allowed
 
-                ${isPrimary ? "bg-scooter-300" : ""}
-                ${isPrimary ? "text-scooter-950" : ""}
-                ${isPrimary ? "hover:bg-scooter-200" : ""}
-                ${isPrimary ? "active:bg-scooter-400" : ""}
+                ${isPrimary ?
+                    `
+                    bg-scooter-300
+                    text-scooter-950
+                    hover:bg-scooter-200
+                    active:bg-scooter-400
+                    `
+                    : ""
+                }
 
-                ${isSecondary ? "bg-trinidad-500" : ""}
-                ${isSecondary ? "text-trinidad-950" : ""}
-                ${isSecondary ? "hover:bg-trinidad-400" : ""}
-                ${isSecondary ? "active:bg-trinidad-600" : ""}
+                ${isSecondary ?
+                    `
+                    bg-trinidad-500
+                    text-trinidad-950
+                    hover:bg-trinidad-400
+                    active:bg-trinidad-600
+                    `
+                    : ""
+                }
 
-                ${isSuccess ? "bg-green-500" : ""}
-                ${isSuccess ? "text-green-950" : ""}
-                ${isSuccess ? "hover:bg-green-400" : ""}
-                ${isSuccess ? "active:bg-green-600" : ""}
+                ${isSuccess ?
+                    `
+                    bg-green-500
+                    text-green-950
+                    hover:bg-green-400
+                    active:bg-green-600
+                    `
+                    : ""
+                }
 
                 ${className ? className : ""}
             `}
