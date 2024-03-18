e7c47fdf6a98c200d740cbcd53243ed61aab23ef
Norton Glitz <norton.glitz@gmail.com>
2024-03-18 16:38:44 -0300

    export all new components

--- components/index.tsx
+++ components/index.tsx
@@ -5,11 +5,16 @@ export { Topbar } from "./topbar"
 export { Button } from "./buttons/button"
 export { ButtonFeedback } from "./buttons/feedback"
 export { ButtonChangeValue } from "./buttons/change-value"
+export { ButtonsOptions } from "./buttons/options"
 
 export { InputText } from "./inputs/text"
+export { InputNumberNumeric } from "./inputs/number/numeric"
 export { InputAvatar } from "./inputs/avatar"
 export { InputSelectMultiple } from "./inputs/selects/multiple"
+export { InputCheckboxOptions } from "./inputs/checkbox/options"
 
 export { Description } from "./typography/description"
 
 export { CardListingProperty } from "./cards/listing-property"
+
+export { FilterPropertyResidentialOwner } from "./filters/property/residential/owner"
